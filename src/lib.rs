2110aafff6f003b38149ab07ff351ca46b3f43d2
cmac4603 <12590206+cmac4603@users.noreply.github.com>
2026-08-08 22:01:23 +0100

    wip: web app

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,4 @@
 #![no_std]
+#![feature(impl_trait_in_assoc_type)]
+
+pub mod web;
---
+++ src/web.rs
@@ -0,0 +1,33 @@
+use esp_alloc as _;
+use picoserve::{routing::get, AppBuilder, AppRouter};
+
+struct AppProps;
+
+impl AppBuilder for AppProps {
+    type PathRouter = impl picoserve::routing::PathRouter;
+
+    fn build_app(self) -> picoserve::Router<Self::PathRouter> {
+        picoserve::Router::new().route("/", get(|| async move { "Hello World" }))
+    }
+}
+
+static CONFIG: picoserve::Config = picoserve::Config::const_default().keep_connection_alive();
+
+const WEB_TASK_POOL_SIZE: usize = 8;
+
+#[embassy_executor::task(pool_size = WEB_TASK_POOL_SIZE)]
+async fn web_task(
+    task_id: usize,
+    stack: embassy_net::Stack<'static>,
+    app: &'static AppRouter<AppProps>,
+) -> ! {
+    let port = 8080;
+    let mut tcp_rx_buffer = [0; 1024];
+    let mut tcp_tx_buffer = [0; 1024];
+    let mut http_buffer = [0; 2048];
+
+    picoserve::Server::new(app, &CONFIG, &mut http_buffer)
+        .listen_and_serve(task_id, stack, port, &mut tcp_rx_buffer, &mut tcp_tx_buffer)
+        .await
+        .into_never()
+}
